3c44ea517e03ec68644bcfb40947b05dc421adf2
cornedev <corneproductions@gmail.com>
2026-04-16 18:40:23 +0200

    Add auth namespace and first function as a start.

--- api/mcapi_auth.cpp
+++ api/mcapi_auth.cpp
@@ -2,4 +2,17 @@
 
 namespace mcapi
 {
+
+namespace auth
+{
+
+std::optional<std::string> GetMicrosoftLoginUrl()
+{
+    const std::string clientid = "3801bb4f-aa98-4355-96a8-8e52ebe042bf";
+
+    return "https://login.live.com/oauth20_authorize.srf?client_id=" + clientid + "&response_type=code&redirect_uri=http://127.0.0.1:8080/&scope=XboxLive.signin%20offline_access";
+}
+
+}
+
 }
